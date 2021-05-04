3e463ac45cb0255a602dea01a43694de10c99a88
fredThem <fdesmarais.them@gmail.com>
2021-05-04 14:57:52 -0400

    ok

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import App from './components/app.jsx';
+
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<Hello name="World" />, root);
+// }
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
